99d6d9758bacfd006420f335f5bf63f710d15638
skippergoroye@gmail.com <oluwatosinadegoroye2@gmail.com>
2023-04-27 13:22:36 -0500

    resume background added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import { BsFillMoonStarsFill } from "react-icons/bs";
+
 
 
 
@@ -13,9 +14,19 @@ export default function Home() {
       </Head>
 
 
-      <main>
-        <section className='bg-blue-700 min-h-screen'>
-          <h1>hello</h1>
+      <main className='bg-white px-10'>
+        <section className='min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl'>developedbyskipper</h1>
+            <ul className='flex items-center'>
+              <li>
+                <BsFillMoonStarsFill className='cursor-pointer text-2xl' />
+              </li>
+              <li>
+                <a href="#" className='bg-cyan-500 text-white px-4 py-2 rounded-full'>Resume</a>
+              </li>
+            </ul>
+          </nav>
         </section>
       </main>
     </div>
