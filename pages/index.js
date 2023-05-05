b244a2215139d4ce9a70c3d8dceffaa6b17c30a4
skippergoroye@gmail.com <oluwatosinadegoroye2@gmail.com>
2023-05-05 03:20:01 +0100

    tech stack added

--- pages/index.js
+++ pages/index.js
@@ -19,13 +19,27 @@ import web6 from "../public/web6.png";
 import { useState } from "react";
 
 
-// Experience
+// Portfolio
 import FacoProject1 from '../assets/portfolio/FacoProject1.png'
 import turinProject from "../assets/portfolio/turinProject.png";
 import hotelmgtProject from "../assets/portfolio/hotelmgtProject.png";
 import podGProject from "../assets/portfolio/podGProject.png";
 import ewalletapi from "../assets/portfolio/ewalletapi.png";
 import queenFisherProject from "../assets/portfolio/queenFisherProject.png";
+
+
+
+
+// Experience
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import javascript from "../assets/javascript.png";
+import NET from "../assets/NET.png";
+import reactImage from "../assets/react.png";
+import nextjs from "../assets/nextjs.png";
+import sqlServe from "../assets/sqlServe.png";
+import github from "../assets/github.png";
+import tailwind from "../assets/tailwind.png";
 
 
 
@@ -62,6 +76,69 @@ export default function Home() {
     {
       id: 6,
       src: ewalletapi,
+    },
+  ];
+
+
+
+
+
+
+// Experience
+  const techs = [
+    {
+      id: 1,
+      src: html,
+      title: "HTML",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 2,
+      src: css,
+      title: "CSS",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 3,
+      src: javascript,
+      title: "JavaScript",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 4,
+      src: reactImage,
+      title: "React",
+      style: "shadow-blue-600",
+    },
+    {
+      id: 5,
+      src: tailwind,
+      title: "Tailwind",
+      style: "shadow-sky-400",
+    },
+    {
+      id: 6,
+      src: nextjs,
+      title: "Next JS",
+      style: "shadow-white",
+    },
+    {
+      id: 7,
+      src: sqlServe,
+      title: "sqlServer",
+      style: "shadow-pink-400",
+    },
+    {
+      id: 8,
+      src: github,
+      title: "GitHub",
+      style: "shadow-gray-400",
+    },
+    {
+      id: 9,
+      src: NET,
+      title: ".NET",
+      style: "shadow-purple-400",
     },
   ];
 
@@ -267,15 +344,50 @@ export default function Home() {
                   </div>
                 </div>
               </div>
-
-
-        </section>
-
+           </section>
 
 
 
 
 
+        {/* <!-----   experinces -------> */}
+        <section>
+            <div
+            name="experience"
+            className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+          >
+            <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
+              <div>
+                <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+                  Experience
+                </p>
+                <p className="py-6">These are the technologies I&apos;ve worked with</p>
+              </div>
+
+             <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+                      {techs.map(({ id, src, title, style }) => (
+                        <div
+                          key={id}
+                          className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+                        >
+                        <Image 
+                            src={src} 
+                            alt="design"
+                            width={'100%'} height={'100%'} 
+                            className="w-20 mx-auto" />
+                        <p className="mt-4">{title}</p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              </section>
+
+
+
+
+
+        {/* <!-----   Form -------> */}
         <section>
         <div
       name="contact"
